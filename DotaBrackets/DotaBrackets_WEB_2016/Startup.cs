f81057ce7a8cf396d1fbcb6c7e7e7136842fa06e
TyCrane <tycrane@ymail.com>
2016-08-15 10:39:45 -0500

    Created Solution

---
+++ DotaBrackets/DotaBrackets_WEB_2016/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DotaBrackets_WEB_2016.Startup))]
+namespace DotaBrackets_WEB_2016
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
